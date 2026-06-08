e677357fa950710bcf845fc835bfa803dd31ff15
PuroSlavKing <103608145+PuroSlavKing@users.noreply.github.com>
2026-06-08 15:29:48 +0000

    chore: Automatically update REUSE headers

--- Modules/Orion/Content.Orion.IntegrationTests/Tests/ModuleLoading/OrionModuleLoadingTest.cs
+++ Modules/Orion/Content.Orion.IntegrationTests/Tests/ModuleLoading/OrionModuleLoadingTest.cs
@@ -1,4 +1,8 @@
-﻿using System.Linq;
+﻿// SPDX-FileCopyrightText: 2026 PuroSlavKing <puroslavking@yahoo.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+using System.Linq;
 using Content.IntegrationTests;
 using Content.IntegrationTests.Fixtures;
 
