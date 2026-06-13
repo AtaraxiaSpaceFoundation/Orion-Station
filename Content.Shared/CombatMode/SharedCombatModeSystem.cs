2b70789ff26b0efe0d107e47c27ce7f7cb9c8414
PuroSlavKing <103608145+PuroSlavKing@users.noreply.github.com>
2026-06-13 18:32:05 +0000

    chore: Automatically update REUSE headers

--- Content.Shared/CombatMode/SharedCombatModeSystem.cs
+++ Content.Shared/CombatMode/SharedCombatModeSystem.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2026 PuroSlavKing <puroslavking@yahoo.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Content.Shared.Actions;
 using Content.Shared.Bed.Sleep;
 using Content.Shared.Mind;
