48f6fd5e386245f054897aea159e3d0e84652b5b
PuroSlavKing <103608145+PuroSlavKing@users.noreply.github.com>
2026-06-17 19:16:13 +0000

    chore: Automatically update REUSE headers

--- Content.IntegrationTests/Utility/GameDataScrounger.cs
+++ Content.IntegrationTests/Utility/GameDataScrounger.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2026 PuroSlavKing <puroslavking@yahoo.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 #nullable enable
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
