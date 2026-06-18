c10152a2c62339e1667ffe20c32c45c0cfe743a0
PuroSlavKing <puroslavking@yahoo.com>
2026-06-18 19:59:57 +0300

    up3.2

--- Content.ModuleManager/ModuleResourceMounter.cs
+++ Content.ModuleManager/ModuleResourceMounter.cs
@@ -11,10 +11,11 @@ public static class ModuleResourceMounter
 {
     public static void MountAll(IResourceManager resourceManager, ISawmill sawmill)
     {
-        var basePath = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "../../"));
-        var modulesPath = Path.Combine(basePath, "Modules");
-        if (!Directory.Exists(modulesPath))
+        // Orion-Edit-Start
+        var modulesPath = FindModulesPath();
+        if (modulesPath is null)
             return;
+        // Orion-Edit-End
 
         var manifests = new List<ModuleManifest>();
         foreach (var manifestPath in Directory.GetFiles(modulesPath, "module.yml", SearchOption.AllDirectories))
@@ -71,4 +72,21 @@ public static class ModuleResourceMounter
                 $"Add it under 'resources:' to mount it, or remove it. Path: {conventionalResources}");
         }
     }
+
+    // Orion-Start
+    private static string? FindModulesPath()
+    {
+        var current = new DirectoryInfo(AppContext.BaseDirectory);
+        while (current is not null)
+        {
+            var modulesPath = Path.Combine(current.FullName, "Modules");
+            if (Directory.Exists(modulesPath))
+                return modulesPath;
+
+            current = current.Parent;
+        }
+
+        return null;
+    }
+    // Orion-End
 }
