c19c571d43b8730a8069bb2d01118185dbca727a
PuroSlavKing <puroslavking@yahoo.com>
2026-06-19 02:33:13 +0300

    Revert "chore: Automatically update REUSE headers"
    This reverts commit 48f6fd5e386245f054897aea159e3d0e84652b5b.

--- Content.IntegrationTests/Utility/GameDataScrounger.cs
+++ Content.IntegrationTests/Utility/GameDataScrounger.cs
@@ -1,7 +1,3 @@
-// SPDX-FileCopyrightText: 2026 PuroSlavKing <puroslavking@yahoo.com>
-//
-// SPDX-License-Identifier: AGPL-3.0-or-later
-
 #nullable enable
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
