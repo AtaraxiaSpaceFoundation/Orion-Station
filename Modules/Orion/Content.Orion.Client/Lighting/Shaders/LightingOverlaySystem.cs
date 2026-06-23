4fc0744f5bf15c97a487c0bcc67b838d1455acb7
PuroSlavKing <103608145+PuroSlavKing@users.noreply.github.com>
2026-06-23 16:55:03 +0000

    chore: Automatically update REUSE headers

--- Modules/Orion/Content.Orion.Client/Lighting/Shaders/LightingOverlaySystem.cs
+++ Modules/Orion/Content.Orion.Client/Lighting/Shaders/LightingOverlaySystem.cs
@@ -1,3 +1,8 @@
+// SPDX-FileCopyrightText: 2026 PuroSlavKing <puroslavking@yahoo.com>
+// SPDX-FileCopyrightText: 2026 ThereDrD <88589686+theredrd0@users.noreply.github.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Robust.Client.Graphics;
 using Robust.Shared.GameObjects;
 using Robust.Shared.IoC;
--- Modules/Orion/Content.Orion.Shared/CCVar/OrionCCVars.Light.cs
+++ Modules/Orion/Content.Orion.Shared/CCVar/OrionCCVars.Light.cs
@@ -1,3 +1,8 @@
+// SPDX-FileCopyrightText: 2026 PuroSlavKing <puroslavking@yahoo.com>
+// SPDX-FileCopyrightText: 2026 ThereDrD <88589686+theredrd0@users.noreply.github.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Robust.Shared.Configuration;
 
 namespace Content.Orion.Shared.CCVar;
