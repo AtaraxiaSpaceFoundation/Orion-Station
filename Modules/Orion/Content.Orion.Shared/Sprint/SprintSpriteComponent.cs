8b04e4162822c1ef05d9dc36576b1e302ed3f8a2
ReWAFFlution <rockstarfly65@gmail.com>
2026-06-25 22:00:13 +0300

    Some fixes

--- Modules/Orion/Content.Orion.Shared/Sprint/SprintSpriteComponent.cs
+++ Modules/Orion/Content.Orion.Shared/Sprint/SprintSpriteComponent.cs
@@ -18,11 +18,11 @@ public sealed partial class SprintSpriteComponent : Component
     /// Interval in seconds between sprite spawns.
     /// </summary>
     [DataField]
-    public float SpawnInterval = 1f;
+    public float SpawnInterval = 0.6f;
+
     /// <summary>
     /// Time accumulated since the last sprite spawn.
     /// </summary>
-    [DataField]
     public float TimeSinceLastSpawn;
 
     /// <summary>
@@ -30,5 +30,5 @@ public sealed partial class SprintSpriteComponent : Component
     /// Must be an entity prototype with a sprite and optional <see cref="Robust.Shared.Spawners.TimedDespawnComponent"/>.
     /// </summary>
     [DataField]
-    public string EffectPrototype = "SprintEffect";
+    public string EntProtoId = "OrionSprintEffect";
 }
